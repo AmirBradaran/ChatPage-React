3ad5068e519cadfd803829c1bc83b3c039d12cab
Amir <baradaran13085@gmail.com>
2024-12-11 14:39:09 +0330

    Set a Style for BodyUser

--- src/Components/FormOfMessage.jsx
+++ src/Components/FormOfMessage.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function FormOfMessage({ Text, Time, Type }) {
   return (
     <div className={`Message ${Type}`}>
+      <h3>
       {Text}
+      </h3>
       <br />
       <time> {Time} </time>
     </div>
